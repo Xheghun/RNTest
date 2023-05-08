91ab694707db34a53f8dd3d63128e39b44c069e6
Xheghun <ysenruf@gmail.com>
2023-05-08 18:46:21 +0100

    add scroll and dynamic list

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, Text, TextInput, View, ScrollView } from 'react-native';
 
 import { styles } from './styles/styles.js'
 
+import { useState } from 'react';
+
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([])
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalInput() {
+    setCourseGoals(currentGoals =>  [...currentGoals, enteredGoalText])
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Enter your course goal'/>
-        <Button title='Add Goal' />
+        <TextInput style={styles.textInput} placeholder='Enter your course goal' onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalInput} />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
+      <ScrollView>
         <Text>List of Goals..</Text>
+        {courseGoals.map((goal) =>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.listItem} key={goal}>{goal}</Text>
+        </View>
+        )}
+      </ScrollView>
       </View>
     </View>
   );
--- styles/styles.js
+++ styles/styles.js
@@ -1,21 +1,44 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export const styles = StyleSheet.create({
     appContainer: {
-        padding: 30,
+        paddingTop: 50,
+        paddingHorizontal: 16,
+        flex: 1
     },
 
     inputContainer: {
+        flex: 1,
         flexDirection: "row",
         justifyContent: "space-between",
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc'
+    },
 
+    goalsContainer: {
+        flex: 6,
     },
 
     textInput: {
         borderWidth: 1,
         borderColor: "#cccccc",
         width: '75%',
-        marginRight: 10,
+        marginRight: 8,
         padding: 6
+    },
+
+
+    listItemContainer: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
+        backgroundColor: "#5e0acc",
+    },
+
+    listItem: {
+        color: "white"
     }
 });
